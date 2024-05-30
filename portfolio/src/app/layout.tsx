0e72a47935b27dee2cbd769d028b70110382cc7f
gnaak <o101srb@daum.net>
2024-05-31 01:28:13 +0900

    test: build test -- no links

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -18,10 +18,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <head>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"
-        />
+
       </head>
       <body className={inter.className}>
         {children}
