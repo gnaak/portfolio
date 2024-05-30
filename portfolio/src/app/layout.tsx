1f2248772d97cc6c88897f04d7d25ed5ccd48738
gnaak <o101srb@daum.net>
2024-05-31 01:02:03 +0900

    text: build test vercel

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
       <head>
         <link
           rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined&display=optional"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"
         />
       </head>
       <body className={inter.className}>
