32d475a3ba751b23650cb56c1d173e103571971c
이근학 <139318007+gnaak@users.noreply.github.com>
2024-06-21 07:49:46 +0900

    fix: portfolio a/s

--- portfolio/src/containers/gnaak/As.tsx
+++ portfolio/src/containers/gnaak/As.tsx
@@ -8,12 +8,9 @@ const As = ({ asRef }: refType) => {
         <div className="flex flex-col xl:gap-3">
           <div className="flex flex-col w-full">
             <div className="flex flex-col gap-1 md:text-base text-sm ">
-              <span>1️⃣ SSR, SEO에 중점을 두고 웹페이지를 제작했습니다.</span>
+              <span>1️⃣ Vercel.app을 이용한 페이지 배포를 배울 수 있었습니다.</span>
               <span>
-                2️⃣ Vercel.app을 이용한 페이지 배포를 배울 수 있었습니다.
-              </span>
-              <span>
-                3️⃣ Lighthouse를 사용하여 웹 최적화에 대해 배울 수 있었습니다.
+                2️⃣ Lighthouse를 사용하여 웹 최적화에 대해 배울 수 있었습니다.
               </span>
             </div>
           </div>
